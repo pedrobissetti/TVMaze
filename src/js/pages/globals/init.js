62390cb2634e930406840b8886282d64bc8e35ea
pedrobissetti <pedrobissetti11@gmail.com>
2020-01-06 16:59:54 -0300

    [DESK/MOB]FuncaoFinalizadaFuncionando-CorrecaoNoHover-do-Header

--- src/js/pages/globals/init.js
+++ src/js/pages/globals/init.js
@@ -3,6 +3,9 @@ import ShowDetail from './_show-detail';
 import PopupDetail from './_pop-details';
 import Favorites from './_favorites';
 
+import outsideClick from './_outsideclick';
+
+
 const init = () => {
   Favorites.init();
   ShowDetail.init();
@@ -13,3 +16,27 @@ const init = () => {
 export default {
   init,
 };
+
+function initMenuMobile() {
+  const mobileButton = document.querySelector('.js--mobile--button');
+  const mobileMenu = document.querySelector('.js--menu--container');
+  const activeClass = 'active';
+  const eventos = ['click', 'touchstart'];
+
+  if (mobileButton) {
+    function openMenu(event) {
+      mobileButton.classList.add('active');
+      mobileMenu.classList.add('active');
+      outsideClick(mobileMenu, eventos, () => {
+        mobileMenu.classList.remove('active');
+        mobileButton.classList.remove('active');
+      })
+    }
+
+    eventos.forEach(evento => {
+      mobileButton.addEventListener(evento, openMenu);
+    });
+  }
+}
+
+initMenuMobile();
